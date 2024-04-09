29209fe534e839fe3dfb5877d29f1a70639dbbf0
sumitjain <sumitjain19.niec@gmail.com>
2024-04-09 23:34:31 +0800

    Feed Feature

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sumit on 09/04/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: Url
+}
