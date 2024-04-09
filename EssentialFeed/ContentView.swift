c2587f93209a13db9cb861f5fd0e96975847413b
Sumit Jain <sumit.jain@foodpanda.com>
2024-04-09 23:09:21 +0800

    Project Setup

---
+++ EssentialFeed/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EssentialFeed
+//
+//  Created by Sumit on 09/04/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
