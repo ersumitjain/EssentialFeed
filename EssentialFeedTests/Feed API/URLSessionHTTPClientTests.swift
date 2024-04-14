45bcd7bba2d19c10417abd4073ddbc0b7e97379e
sumitjain <sumitjain19.niec@gmail.com>
2024-04-14 22:39:50 +0800

    Guarantee correct URL and HTTP method on get requests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -28,6 +28,24 @@ class URLSessionHTTPClient: HTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_performGetRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "http://any-url.com")!
+        
+        let exp = expectation(description: "Wait for request")
+        URLProtocolStub.observeRequest { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        let sut = URLSessionHTTPClient()
+        sut.get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
@@ -56,9 +74,17 @@ class URLSessionHTTPClientTests: XCTestCase {
 
 }
 
+//extension URLSessionHTTPClientTests: CustomDebugStringConvertible {
+//    public override var description: String {
+//        return ""
+//    }
+//}
+
 private class URLProtocolStub: URLProtocol {
     
     private static var stub: Stub?
+    
+    private static var requestObserver: ((URLRequest) -> Void)?
     
     private struct Stub {
         let data: Data?
@@ -77,9 +103,15 @@ private class URLProtocolStub: URLProtocol {
     static func stopInterceptingRequests() {
         URLProtocol.unregisterClass(URLProtocolStub.self)
         stub = nil
+        requestObserver = nil
+    }
+    
+    static func observeRequest(observer: @escaping(URLRequest)-> Void) {
+        requestObserver = observer
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
+        requestObserver?(request)
         return true
     }
     
