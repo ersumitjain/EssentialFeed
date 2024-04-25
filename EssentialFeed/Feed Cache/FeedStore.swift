331b181c723a67fe4827ed1188d464b1e7062a86
sumitjain <sumitjain19.niec@gmail.com>
2024-04-25 21:11:40 +0800

    Move FeedStore to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,18 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Sumit on 25/04/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+}
