030f0d7f4c55a3555cca27751346b9456c333ea3
sumitjain <sumitjain19.niec@gmail.com>
2024-05-10 00:47:57 +0800

    Create FeedStoreSpecs that must be implemented by any FeedStore test implementation to guarantee it meets spec

---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -0,0 +1,38 @@
+//
+//  FeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Sumit on 10/05/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import Foundation
+
+protocol FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache()
+     func test_retrieve_hasNoSideEffectsOnEmptyCache()
+     func test_retrieve_deliversFoundValueOnNonEmptyCache()
+     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+   
+
+     func test_insert_overridesPreviouslyInsertedCacheValues()
+   
+
+     func test_delete_hasNoSideEffectsOnEmptyCache()
+     func test_delete_emptiesPreviouslyInsertedCache()
+    
+     func test_storeSideEffects_runSerially()
+}
+
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+     func test_retrieve_deliversFailureOnRetrievalError()
+     func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
+     func test_insert_deliversErrorOnInsertionError()
+     func test_insert_hasNoSideEffectOnInsertionError()
+}
+
+//protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
+//}
