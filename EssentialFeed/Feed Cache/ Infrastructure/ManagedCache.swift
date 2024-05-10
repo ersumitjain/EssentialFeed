1b2e90c34ba0e77297de05f86e352004c5f11f43
sumitjain <sumitjain19.niec@gmail.com>
2024-05-11 00:21:27 +0800

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeed/Feed Cache/ Infrastructure/ManagedCache.swift
@@ -0,0 +1,30 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Sumit on 11/05/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged internal var timestamp: Date
+    @NSManaged internal var feed: NSOrderedSet
+
+    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+
+    internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+
+    internal var localFeed: [LocalFeedItem] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}
---
+++ EssentialFeed/Feed Cache/ Infrastructure/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Sumit on 11/05/24.
+//  Copyright © 2024 Sumit. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+    @NSManaged internal var id: UUID
+    @NSManaged internal var imageDescription: String?
+    @NSManaged internal var location: String?
+    @NSManaged internal var url: URL
+    @NSManaged internal var cache: ManagedCache
+
+    internal static func images(from localFeed: [LocalFeedItem], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.imageUrl
+            return managed
+        })
+    }
+
+    internal var local: LocalFeedItem {
+        return LocalFeedItem(id: id, description: imageDescription, location: location, imageUrl: url)
+    }
+}
